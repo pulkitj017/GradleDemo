46fe9440f34d8e46fb58f55c2c3c5836bf32d85d
pulkitj017 <pulkit.jain8004@thepsi.com>
2025-09-16 12:03:00 +0530

    Create trivy.sh

---
+++ trivy.sh
@@ -0,0 +1,10 @@
+
+INSTALL_DIR="$HOME/bin"
+PROJECT_DIR=$(pwd)
+# Install Trivy
+
+echo "Installing Trivy..."
+sudo curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b "$INSTALL_DIR" v0.50.4
+
+# Run Trivy to scan for vulnerabilities and output to trivy-vulnerabilities.txt
+"$INSTALL_DIR/trivy" fs --scanners vuln --format table --output trivy-vulnerabilities.txt "$PROJECT_DIR"
